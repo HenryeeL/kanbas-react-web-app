28ef9e8401680ca24f7caf419efa51c914afd354
HenryeeL <l853490206@qq.com>
2024-09-19 23:44:00 -0700

    A1 ver@1.2

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
 import Labs from "./Labs";
 import Kanbas from "./Kanbas";
+import LandingPage from './Landing';
+import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
 export default function App() {
   return (
     <HashRouter>
       <div>
         <Routes>
-          <Route path="/" element={<Navigate to="Labs" />} />
+          <Route path="/" element={<LandingPage />} />
           <Route path="/Labs/*" element={<Labs />} />
           <Route path="/Kanbas/*" element={<Kanbas />} />
         </Routes>
